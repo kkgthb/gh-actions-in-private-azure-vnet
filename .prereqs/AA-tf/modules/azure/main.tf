592fb32844fe09bcc69ac11be017b408cf594877
K. <k@example.com>
2026-03-13 12:27:37 -0500

    TF AZ resource group create

--- .prereqs/AA-tf/modules/azure/main.tf
+++ .prereqs/AA-tf/modules/azure/main.tf
@@ -1,1 +1,8 @@
+# "Hello world" query I can remove later
 data "azurerm_client_config" "current_azrm_context" {}
+
+# Azure resource group to hold it all
+resource "azurerm_resource_group" "my_resource_group" {
+  name     = "${var.workload_nickname}-rg-demo"
+  location = "centralus"
+}
