df53304ffa8e888ddac8529483d1849ebe550518
K. <k@example.com>
2026-03-13 10:29:27 -0500

    Successfully TF queried the GH org I have access to

---
+++ .prereqs/AA-tf/variables.tf
@@ -0,0 +1,12 @@
+variable "entra_tenant_id" {
+  type = string
+}
+variable "az_sub_id" {
+  type = string
+}
+variable "workload_nickname" {
+  type = string
+}
+variable "gh_org_name" {
+  type = string
+}
