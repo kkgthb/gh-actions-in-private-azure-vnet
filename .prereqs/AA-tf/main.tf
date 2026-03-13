f15758cb2f2cf2f967ca8b855bfc1158ebd9293c
K. <k@example.com>
2026-03-13 10:34:48 -0500

    Moved GH org query folder structure

--- .prereqs/AA-tf/main.tf
+++ .prereqs/AA-tf/main.tf
@@ -1,3 +1,8 @@
-data "github_organization" "my_gh_org" {
-  name = var.gh_org_name
+module "github_org_config" {
+  source = "./modules/github/orgconfig"
+  providers = {
+    github = github.demo
+  }
+  workload_nickname = var.workload_nickname
+  gh_org_name       = var.gh_org_name
 }
---
+++ .prereqs/AA-tf/modules/github/orgconfig/main.tf
@@ -0,0 +1,3 @@
+data "github_organization" "my_gh_org" {
+  name = var.gh_org_name
+}
